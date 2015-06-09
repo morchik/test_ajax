c00ca65127c8ea523a6bf415d92ec63ffdc70bb6
admin <admin@admin-PC>
2015-06-09 20:11:38 +0700

    ajax try application/json; charset=utf-8

--- test/src/com/example/test/HttpClient.java
+++ test/src/com/example/test/HttpClient.java
@@ -6,7 +6,24 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.util.Log;
+
 public class HttpClient {
+	
+	// imitate chrome
+	public void setRequestProperty(HttpURLConnection con) {
+			con.setRequestProperty("Accept", "application/json; charset=utf-8"); // ajax application/json; charset=utf-8
+			con.setRequestProperty("Accept-Encoding", "deflate");
+			con.setRequestProperty("Accept-Language",
+					"ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
+			con.setRequestProperty("Cache-Control", "no-cache");
+			con.setRequestProperty("Connection", "keep-alive");
+			con.setRequestProperty("DNT", "1");
+			con.setRequestProperty("Pragma", "no-cache");
+			con.setRequestProperty(
+					"User-Agent",
+					"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36");
+		}
 
 	public String getData(String l_url) {
 		HttpURLConnection con = null;
@@ -16,9 +33,14 @@ public class HttpClient {
 			con = (HttpURLConnection) (new URL(l_url))
 					.openConnection();
 			con.setRequestMethod("POST");
+			
+			setRequestProperty(con);
 			con.setDoInput(true);
-			con.setDoOutput(true);
-			con.connect();
+			con.setDoOutput(false);
+			con.setConnectTimeout(60000);
+			con.connect();	
+			int stt = con.getResponseCode();
+			Log.d("HttpClient", "html getResponseCode " + stt+" "+l_url);
 			StringBuffer buffer = new StringBuffer();
 			is = con.getInputStream();
 			BufferedReader br = new BufferedReader(new InputStreamReader(is));
