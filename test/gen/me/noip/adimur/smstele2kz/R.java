c926683dc08b0e31ccecb902a2d0d92eb9a1f0f3
admin <admin@admin-PC>
2015-11-08 07:07:38 +0700

    v 009 ui new

--- test/gen/me/noip/adimur/smstele2kz/R.java
+++ test/gen/me/noip/adimur/smstele2kz/R.java
@@ -28,14 +28,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher0=0x7f020003;
     }
     public static final class id {
-        public static final int dummy_button=0x7f080006;
-        public static final int edMessage=0x7f080002;
-        public static final int edNumber=0x7f080003;
+        public static final int dummy_button=0x7f080002;
+        public static final int edMessage=0x7f080004;
+        public static final int edNumber=0x7f080005;
         public static final int fullscreen_content=0x7f080000;
-        public static final int fullscreen_content_controls=0x7f080005;
-        public static final int sett_button=0x7f080007;
-        public static final int tvDebug=0x7f080004;
-        public static final int tvStatus=0x7f080001;
+        public static final int fullscreen_content_controls=0x7f080001;
+        public static final int sett_button=0x7f080003;
+        public static final int tvDebug=0x7f080006;
+        public static final int tvStatus=0x7f080007;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
